bb68840459457b9c7ec00efc9dc28e1d73af84ce
Bleedem <jjacobwow@gmail.com>
2023-10-25 19:46:34 -0400

    edits

---
+++ script.js
@@ -0,0 +1,11 @@
+const wpp = document.getElementById("home-page");
+const wallpaper = document.querySelector(".main").offsetTop;
+window.onscroll = function() {
+    if (window.scrollY > 0) {
+        var opac = ((window.scrollY / wallpaper) * 1.8);
+        var translateY = (window.scrollY * -.5);
+      console.log(opac, translateY);
+        wpp.style.backgroundPositionY = translateY + "px !important";
+        wpp.style.background = "linear-gradient(rgba(16, 16, 16, " + opac + "), rgba(16, 16, 16, " + opac + ")), url(https://cloudfront-us-east-1.images.arcpublishing.com/tgam/H4XPBBRKHFMTTLXEBVBLK65HOM.jpg) no-repeat";
+    }
+}
